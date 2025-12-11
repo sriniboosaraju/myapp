e065bfd0f6a3399ec07706cd3cd5ad8007982631
Srinivas Boosaraju <srini.boosaraju@gmail.com>
2025-12-11 23:07:44 +0530

    made changes to index

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.json({ hello: "srinivas", commit: process.env.GIT_COMMIT || 'local' });
+  res.json({ hello: "sritest", commit: process.env.GIT_COMMIT || 'local' });
 });
 app.listen(port, () => console.log(`listening on ${port}`));
