0f2bfa98176ed874ad41fd46646fe1b729282359
Srinivas Boosaraju <srini.boosaraju@gmail.com>
2025-12-12 00:21:48 +0530

    Update to oct-testing-sample-app

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.json({ hello: "oct-testing-sample-app", commit: process.env.GIT_COMMIT || 'local' });
+  res.json({ hello1234: "oct-testing-sample-app1234", commit: process.env.GIT_COMMIT || 'local' });
 });
 app.listen(port, () => console.log(`listening on ${port}`));
