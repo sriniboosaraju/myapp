a4f941f05a3be2d7d70dc839a198fa3b0b8ca99d
sriniboosaraju <148060757+sriniboosaraju@users.noreply.github.com>
2026-01-13 21:08:12 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.json({ hello12345: "Jermy", commit: process.env.GIT_COMMIT || 'local' });
+  res.json({ hello12345: "Ryan-amy", commit: process.env.GIT_COMMIT || 'local' });
 });
 app.listen(port, () => console.log(`listening on ${port}`));
